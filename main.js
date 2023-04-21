1e71021bd46c64442fa229d2fc90e4693b56e801
COEX <CORREO@example.com>
2023-04-21 08:21:45 -0500

    rama 2 finalizada

--- main.js
+++ main.js
@@ -1,32 +1,46 @@
-const num1 = document.getElementById('num1')
-const num2 = document.getElementById('num2')
+const num = document.getElementById('num1')
 const operando = document.getElementById('operando')
 const display = document.getElementById('display')
 const botones = document.querySelector('.calculadora__botones')
+let num1 = ""
 
 botones.addEventListener('click',(evento)=>{
-    if (isNaN(num1.value) && isNaN(num2.value)){
-        display.textContent = 'Error'
+    if (isNaN(num.value)){
         alert('Ingrese un número')
-    }else if(evento.target.id=='suma'){
+    }
+    else if(evento.target.id=='suma'){
         operando.textContent= '+'
+        num1 += num.value
+        num.value = ' '
     }else if(evento.target.id=='rest'){
         operando.textContent= '-'
+        num1 += num.value
+        num.value = ' '
     }else if(evento.target.id=='divi'){
         operando.textContent= '/'
+        num1 += num.value
+        num.value = ' '
     }else if(evento.target.id=='mult'){
         operando.textContent= '*'
+        num1 += num.value
+        num.value = ' '
+    }else if(evento.target.id=='limpiar'){
+        num.value=''
+        operando.textContent=''
+        display.textContent=''   
     }
+    
 // 
     if(evento.target.id == 'igual'){
         if(operando.textContent == '+'){
-            display.textContent = (+num1.value + +num2.value)
+            display.textContent = (+num1 + +num.value)
         }else if(operando.textContent== '-'){
-            display.textContent = (+num1.value - +num2.value)
+            display.textContent = (+num1 - +num.value)
         }else if(operando.textContent == '/'){
-            display.textContent = (+num1.value / +num2.value)
+            display.textContent = (+num1 / +num.value)
         }else if (operando.textContent == '*'){
-            display.textContent = (+num1.value * +num2.value)
+            display.textContent = (+num1 * +num.value)
         }
     }
 })
+
